0e3b73ac2dd31539765e3e85eb503c8e2c4e2b0c
friedenvillain <villain@kimo.com>
2016-08-10 10:58:25 +0800

    Update UNO.ino

--- pen-NANO/UNO.ino
+++ pen-NANO/UNO.ino
@@ -15,6 +15,8 @@ void setup() {
   BT.begin(9600);
 
   myservo.attach(A5, 400, 2400); // 修正伺服馬達脈衝寬度範圍
+  //myservo.write(90); // 一開始先置中90度
+  //delay(3000);
 }
 
 void loop() {
@@ -39,6 +41,7 @@ void loop() {
           for (int i = 1200; i <= 1602; i += 3) { //數字大的是下筆
             myservo.write(i); // 直接以脈衝寬度控制
             delay(1);//速度
+          }
           for (int i = 1602; i <= 1656; i += 3) { //數字大的是下筆
             myservo.write(i); // 直接以脈衝寬度控制
             delay(5);//速度
@@ -49,10 +52,10 @@ void loop() {
           for (int i = 1656; i >= 1254; i -= 3) {
             myservo.write(i);
             delay(1);
+          }
             for (int i = 1254; i >= 1200; i -= 3) {
             myservo.write(i);
             delay(5);
-           
           }
           Serial.println("up");
           break;
